41d6ca02b729fca6a5ab80cb05392e3da612c32d
Puchok-93 <paul.puchkov@yandex.ru>
2020-02-03 23:54:32 +0300

    Исправлены ошибки вывода статистики + линтер

--- js/stat.js
+++ js/stat.js
@@ -1,69 +1,64 @@
+'use strict';
+
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
-
-var renderCloud = function(ctx, x, y, color) {
+var HISTOGRAM_HEIGTH = 150;
+var HISTOGRAM_X = 150;
+var HISTOGRAM_Y = 245;
+var COLUMN_MARGIN = 90;
+var COLUMN_WIDTH = 40;
+var COLUMN_REVERSE = -1;
+var LINE_HEIGHT = 20;
+var USER_COLOR = 'rgba(255, 0, 0, 1)';
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 
 };
 
- /*Определение цвета*/
-
- var getPlayerColor = function (playerName) {
-  if (playerName === 'Вы') {
-    return 'rgba(255, 0, 0, 1)';
-  } else {
-    return 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, 50%';
-  }
+// Определение цвета
+var getPlayerColor = function (playerName) {
+  return (playerName === 'Вы') ? USER_COLOR : 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, 50%';
 };
 
-  /*Определение максимального значения*/
+// Определение максимального значения
 
-  var getMaxValue = function (anyArray) {
-    var maxValue = 0;
-
-    for (var i= 0; i < anyArray.length; i++) {
-      if (anyArray[i] > maxValue) {
-        maxValue = anyArray[i];
-      }
+var getMaxValue = function (values) {
+  var maxValue = 0;
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] > maxValue) {
+      maxValue = values[i];
     }
-    return maxValue;
-  };
+  }
+  return maxValue;
+};
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  /*Отрисовка текста*/
+  // Отрисовка текста
 
   ctx.fillStyle = '#000';
   ctx.font = '16px Tahoma';
   ctx.fillText('Ура вы победили!', 135, 35);
   ctx.fillText('Список результатов:', 135, 60);
 
-  /*Построение гистограммы*/
-
-  var HISTOGRAM_HEIGTH = 150;
-  var HISTOGRAM_X = 150;
-  var HISTOGRAM_Y = 245;
-  var COLUMN_MARGIN = 90;
-  var COLUMN_WIDTH = 40;
-  var COLUMN_REVERSE = -1;
-  var LINEHEIGHT = 20;
+  // Построение гистограммы
   var maxTime = getMaxValue(times);
 
-  for (j = 0; j < times.length; j++) {
-    var playerTime = Math.round(times[j]);
+  for (var size = 0; size < times.length; size++) {
+    var playerTime = Math.round(times[size]);
     var columnHeight = playerTime * HISTOGRAM_HEIGTH / maxTime;
 
-    ctx.fillStyle = getPlayerColor(names[j]);
-    ctx.fillRect(HISTOGRAM_X + j * COLUMN_MARGIN, HISTOGRAM_Y, COLUMN_WIDTH, columnHeight * COLUMN_REVERSE);
+    ctx.fillStyle = getPlayerColor(names[size]);
+    ctx.fillRect(HISTOGRAM_X + size * COLUMN_MARGIN, HISTOGRAM_Y, COLUMN_WIDTH, columnHeight * COLUMN_REVERSE);
 
     ctx.fillStyle = '#000000';
-    ctx.fillText(playerTime, HISTOGRAM_X + j * COLUMN_MARGIN, HISTOGRAM_Y - columnHeight - LINEHEIGHT / 2);
-    ctx.fillText(names[j], HISTOGRAM_X + j * COLUMN_MARGIN, HISTOGRAM_Y + LINEHEIGHT);
+    ctx.fillText(playerTime, HISTOGRAM_X + size * COLUMN_MARGIN, HISTOGRAM_Y - columnHeight - LINE_HEIGHT / 2);
+    ctx.fillText(names[size], HISTOGRAM_X + size * COLUMN_MARGIN, HISTOGRAM_Y + LINE_HEIGHT);
   }
 };
