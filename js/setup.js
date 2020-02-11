e5369f0bec34b6e11d4ef8ae54ac150b37e4b65a
Puchok-93 <paul.puchkov@yandex.ru>
2020-02-11 20:18:33 +0300

    Добавление функции getFragment

--- js/setup.js
+++ js/setup.js
@@ -2,18 +2,18 @@
 // Массивы
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
 // Генерируем рандомное число
 var getRandomElement = function (elements) {
   return elements[Math.floor(Math.random() * elements.length)];
 };
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+var similarWindow = document.querySelector('.setup');
+similarWindow.classList.remove('hidden');
 
-var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarListElement = similarWindow.querySelector('.setup-similar-list');
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
@@ -24,8 +24,8 @@ var getRandomWizard = function () {
   var randomWizard =
     {
       name: getRandomElement(WIZARD_NAMES) + ' ' + getRandomElement(WIZARD_SURNAMES),
-      coatColor: getRandomElement(COAT_COLORS),
-      eyesColor: getRandomElement(EYES_COLORS)
+      coatColor: getRandomElement(WIZARD_COAT_COLORS),
+      eyesColor: getRandomElement(WIZARD_EYES_COLORS)
     };
 
   return randomWizard;
@@ -54,10 +54,14 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < createWizards.length; i++) {
-  fragment.appendChild(renderWizard(createWizards[i]));
-}
-similarListElement.appendChild(fragment);
+var getFragment = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < createWizards.length; i++) {
+    fragment.appendChild(renderWizard(createWizards[i]));
+  }
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  return fragment;
+};
+similarListElement.appendChild(getFragment());
+
+similarWindow.querySelector('.setup-similar').classList.remove('hidden');
