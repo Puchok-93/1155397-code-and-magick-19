70b49bbe86b42fe0f9e1a86fc47030f0b648e9db
Puchok-93 <paul.puchkov@yandex.ru>
2020-02-22 15:08:00 +0300

    Добавление обработчиков событий. Открытие-закрытие окно + ред. персонажа

--- js/setup.js
+++ js/setup.js
@@ -4,14 +4,115 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
+// ------------------------------------------Настройка персонажа / Обработка событий------------------------------------------
+
+// Избавляемся от магических чисел
+var ESC_KEY = 'Escape';
+var ENTER_KEY = 'Enter';
+
+var similarWindow = document.querySelector('.setup');
+var similarWindowOpen = document.querySelector('.setup-open');
+var similarWindowClose = similarWindow.querySelector('.setup-close');
+var similarWindowUserName = similarWindow.querySelector('.setup-user-name');
+
+var setupPlayer = document.querySelector('.setup-player');
+var setupWizardAppearance = setupPlayer.querySelector('.setup-wizard-appearance');
+
+var wizardEyes = document.querySelector('.wizard-eyes');
+var wizardCoat = document.querySelector('.wizard-coat');
+var setupFireballWrap = setupPlayer.querySelector('.setup-fireball-wrap');
+var fireball = setupFireballWrap.querySelector('.setup-fireball');
+
+var wizardEyesColorInput = setupWizardAppearance.querySelector('input[name=eyes-color]');
+var wizardCoatColorInput = setupWizardAppearance.querySelector('input[name=coat-color]');
+var fireballColorInput = setupFireballWrap.querySelector('input[name=fireball-color');
+
+var onUserNameFocus = function () {
+  document.removeEventListener('keydown', onSimilarWindowEscPress);
+};
+
+var onUserNameBlur = function () {
+  document.addEventListener('keydown', onSimilarWindowEscPress);
+};
+
+// Выбираем случайный элемент из массива и подставляем к нужным элементам
+var changeColor = function (array, element, input) {
+  var color = getRandomElement(array);
+  element.style.fill = color;
+  input.value = color;
+};
+
+// Смена цвета глаз при клике
+var onEyesClick = function () {
+  changeColor(WIZARD_EYES_COLORS, wizardEyes, wizardEyesColorInput);
+};
+
+// Смена цвета мантии при клике
+var onCoatClick = function () {
+  changeColor(WIZARD_COAT_COLORS, wizardCoat, wizardCoatColorInput);
+};
+
+// Смена цвета фаерболла при клике
+var onFireballClick = function () {
+  var colorFireball = getRandomElement(FIREBALL_COLORS);
+  setupFireballWrap.style.backgroundColor = colorFireball;
+  fireballColorInput.value = colorFireball;
+};
+
+
+var onSimilarWindowEscPress = function (evt) {
+  if (evt.key === ESC_KEY) {
+    closeSimilarWindow();
+  }
+};
+
+similarWindowOpen.addEventListener('click', function () {
+  openSimilarWindow();
+});
+
+similarWindowOpen.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    similarWindow.classList.remove('hidden');
+  }
+});
+
+similarWindowClose.addEventListener('click', function () {
+  closeSimilarWindow();
+});
+
+similarWindowClose.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    similarWindow.classList.add('hidden');
+  }
+});
+
+var openSimilarWindow = function () {
+  similarWindow.classList.remove('hidden');
+  document.addEventListener('keydown', onSimilarWindowEscPress);
+  similarWindowUserName.addEventListener('focusin', onUserNameFocus);
+  similarWindowUserName.addEventListener('focusout', onUserNameBlur);
+  wizardEyes.addEventListener('click', onEyesClick);
+  wizardCoat.addEventListener('click', onCoatClick);
+  fireball.addEventListener('click', onFireballClick);
+};
+
+var closeSimilarWindow = function () {
+  similarWindow.classList.add('hidden');
+  document.removeEventListener('keydown', onSimilarWindowEscPress);
+  similarWindowUserName.removeEventListener('focusin', onUserNameFocus);
+  similarWindowUserName.removeEventListener('focusout', onUserNameBlur);
+  wizardEyes.removeEventListener('click', onEyesClick);
+  wizardCoat.removeEventListener('click', onCoatClick);
+  fireball.removeEventListener('click', onFireballClick);
+};
+
+// ------------------------------------------Настройка персонажа / Обработка событий------------------------------------------
 // Генерируем рандомное число
 var getRandomElement = function (elements) {
   return elements[Math.floor(Math.random() * elements.length)];
 };
-
-var similarWindow = document.querySelector('.setup');
-similarWindow.classList.remove('hidden');
 
 var similarListElement = similarWindow.querySelector('.setup-similar-list');
 
@@ -65,3 +166,5 @@ var getFragment = function () {
 similarListElement.appendChild(getFragment());
 
 similarWindow.querySelector('.setup-similar').classList.remove('hidden');
+
+// ------------------------------------------Настройка персонажа / Обработка событий------------------------------------------
